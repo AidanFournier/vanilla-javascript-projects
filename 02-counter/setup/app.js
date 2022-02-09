769fe0e415a6098503953079069fa8b8e0e938c8
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-02-09 21:39:55 +0900

    Created counter

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -5,3 +5,25 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--; // decreasing count by 1
+        } else if(styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if(count > 0) {
+            value.style.color = "green";
+        }
+        if(count < 0) {
+            value.style.color = "red";
+        }
+        if(count === 0) {
+            value.style.color = "black"; // or #222
+        }
+        value.textContent = count; // changes the number that appears
+    })
+} )
