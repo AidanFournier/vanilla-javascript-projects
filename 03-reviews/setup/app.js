2bac89666fe56d947be06a2e9aecb8337f874d26
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-02-12 23:53:04 +0900

    Added toggle and random review

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -47,3 +47,46 @@ const info = document.getElementById("info");
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// once document loads, display first item/initial value (right now, everything is hard-coded)
+// load initial item
+window.addEventListener('DOMContentLoaded', function() {
+  // console.log("shake and bake");
+  showPerson(currentItem);
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', function() {
+  currentItem++;
+  if(currentItem > reviews.length - 1) {// -1 reflects the last item in array
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// show previous person
+prevBtn.addEventListener('click', function() {
+  currentItem--;
+  if(currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener('click', function() {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+})
